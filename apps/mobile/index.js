643edf47fc3a461fd592b7920cb3386f147c1c44
Hiten Patel <hitenpatel2010@gmail.com>
2026-03-22 08:56:46 +0000

    fix: polyfill SharedArrayBuffer with ArrayBuffer for Hermes Android

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,3 +1,9 @@
+// Polyfill SharedArrayBuffer for Hermes (required by PowerSync/SQLite)
+// Hermes on Android debug builds doesn't support SharedArrayBuffer natively
+if (typeof globalThis.SharedArrayBuffer === "undefined") {
+  globalThis.SharedArrayBuffer = ArrayBuffer;
+}
+
 import { registerRootComponent } from "expo";
 import App from "./App";
 
